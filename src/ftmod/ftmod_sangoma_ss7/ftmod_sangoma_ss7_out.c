0781cd8499214df3694fc3a5ddfefb7f1b5ee4a3
James Zhang <jzhang@sangoma.com>
2012-10-17 16:27:37 -0400

    revert last change

--- src/ftmod/ftmod_sangoma_ss7/ftmod_sangoma_ss7_out.c
+++ src/ftmod/ftmod_sangoma_ss7/ftmod_sangoma_ss7_out.c
@@ -595,6 +595,7 @@ void ft_to_sngss7_blo (ftdm_channel_t * ftdmchan)
 	
 	sngss7_chan_data_t *sngss7_info = ftdmchan->call_data;
 	
+	sngss7_set_cmd_pending_flag(sngss7_info, FLAG_CMD_PENDING_WAIT_FOR_RX_BLA);
 	sng_cc_sta_request (1,
 						0,
 						0,
@@ -603,7 +604,6 @@ void ft_to_sngss7_blo (ftdm_channel_t * ftdmchan)
 						SIT_STA_CIRBLOREQ, 
 						NULL);
 	
-	sngss7_set_cmd_pending_flag(sngss7_info, FLAG_CMD_PENDING_WAIT_FOR_RX_BLA);
 	SS7_INFO_CHAN(ftdmchan,"[CIC:%d]Tx BLO\n", sngss7_info->circuit->cic);
 
 	SS7_FUNC_TRACE_EXIT (__FUNCTION__);
@@ -643,6 +643,8 @@ ft_to_sngss7_ubl (ftdm_channel_t * ftdmchan)
 	if (sngss7_test_cmd_pending_flag(sngss7_info, FLAG_CMD_PENDING_WAIT_FOR_RX_BLA) ) {
 		sngss7_set_cmd_pending_flag(sngss7_info, FLAG_CMD_PENDING_WAIT_FOR_TX_UBL);
 		SS7_INFO_CHAN(ftdmchan, "[CIC:%d]Set pending UBL request on Rx BLA.\n",	sngss7_info->circuit->cic);
+		SS7_FUNC_TRACE_EXIT (__FUNCTION__);
+		return;
 	} 
 
 	sng_cc_sta_request (1,
