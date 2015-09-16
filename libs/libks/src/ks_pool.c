0b242ff8c017cfd619f52a26e69c4896faf4882d
Michael Jerris <mike@jerris.com>
2015-09-16 12:56:42 -0400

    remove extra private header file, just move it into the c file and simplify complex include chain that was not necessary

--- libs/libks/src/ks_pool.c
+++ libs/libks/src/ks_pool.c
@@ -52,14 +52,113 @@
 #endif
 #include <sys/mman.h>
 
-#ifdef DMALLOC
-#include "dmalloc.h"
-#endif
+#define KS_MPOOL_MAGIC	0xABACABA	/* magic for struct */
+#define BLOCK_MAGIC	0xB1B1007	/* magic for blocks */
+#define FENCE_MAGIC0	(unsigned char)(0xFAU)	/* 1st magic mem byte */
+#define FENCE_MAGIC1	(unsigned char)(0xD3U)	/* 2nd magic mem byte */
 
-#define KS_MPOOL_MAIN
+#define FENCE_SIZE		2		/* fence space */
+#define MIN_ALLOCATION		(sizeof(ks_mpool_free_t))	/* min alloc */
+#define MAX_FREE_SEARCH		10240	/* max size to search */
+#define MAX_FREE_LIST_SEARCH	100	/* max looking for free mem */
 
-#include "ks_pool.h"
-#include "ks_pool_loc.h"
+#define PRE_MAGIC1 0x33U
+#define PRE_MAGIC2 0xCCU
+
+typedef struct alloc_prefix_s {
+	unsigned char m1;
+	unsigned long size;
+	unsigned char m2;
+} alloc_prefix_t;
+
+#define PREFIX_SIZE sizeof(struct alloc_prefix_s)
+
+/*
+ * bitflag tools for Variable and a Flag
+ */
+#define BIT_FLAG(x)		(1 << (x))
+#define BIT_SET(v,f)		(v) |= (f)
+#define BIT_CLEAR(v,f)		(v) &= ~(f)
+#define BIT_IS_SET(v,f)		((v) & (f))
+#define BIT_TOGGLE(v,f)		(v) ^= (f)
+
+#define SET_POINTER(pnt, val)					\
+	do {										\
+		if ((pnt) != NULL) {					\
+			(*(pnt)) = (val);					\
+		}										\
+	} while(0)
+
+#define BLOCK_FLAG_USED		BIT_FLAG(0)	/* block is used */
+#define BLOCK_FLAG_FREE		BIT_FLAG(1)	/* block is free */
+
+#define DEFAULT_PAGE_MULT		16	/* pagesize = this * getpagesize */
+
+/* How many pages SIZE bytes resides in.  We add in the block header. */
+#define PAGES_IN_SIZE(mp_p, size)	(((size) + sizeof(ks_mpool_block_t) +	\
+									  (mp_p)->mp_page_size - 1) /		\
+									 (mp_p)->mp_page_size)
+#define SIZE_OF_PAGES(mp_p, page_n)	((page_n) * (mp_p)->mp_page_size)
+#define MAX_BITS	30			/* we only can allocate 1gb chunks */
+
+#define MAX_BLOCK_USER_MEMORY(mp_p)	((mp_p)->mp_page_size - \
+									 sizeof(ks_mpool_block_t))
+#define FIRST_ADDR_IN_BLOCK(block_p)	(void *)((char *)(block_p) +	\
+												 sizeof(ks_mpool_block_t))
+#define MEMORY_IN_BLOCK(block_p)	((char *)(block_p)->mb_bounds_p -	\
+									 ((char *)(block_p) +				\
+									  sizeof(ks_mpool_block_t)))
+
+typedef struct ks_mpool_cleanup_node_s {
+	ks_mpool_cleanup_fn_t fn;
+	void *ptr;
+	void *arg;
+	int type;
+	struct ks_mpool_cleanup_node_s *next;
+} ks_mpool_cleanup_node_t;
+
+struct ks_mpool_s {
+	unsigned int mp_magic;		/* magic number for struct */
+	unsigned int mp_flags;		/* flags for the struct */
+	unsigned long mp_alloc_c;	/* number of allocations */
+	unsigned long mp_user_alloc;	/* user bytes allocated */
+	unsigned long mp_max_alloc;	/* maximum user bytes allocated */
+	unsigned int mp_page_c;		/* number of pages allocated */
+	unsigned int mp_max_pages;	/* maximum number of pages to use */
+	unsigned int mp_page_size;	/* page-size of our system */
+	off_t mp_top;				/* top of our allocations in fd */
+	ks_mpool_log_func_t mp_log_func;	/* log callback function */
+	void *mp_addr;				/* current address for mmaping */
+	void *mp_min_p;				/* min address in pool for checks */
+	void *mp_bounds_p;			/* max address in pool for checks */
+	struct ks_mpool_block_st *mp_first_p;	/* first memory block we are using */
+	struct ks_mpool_block_st *mp_last_p;	/* last memory block we are using */
+	struct ks_mpool_block_st *mp_free[MAX_BITS + 1];	/* free lists based on size */
+	unsigned int mp_magic2;		/* upper magic for overwrite sanity */
+	ks_mpool_cleanup_node_t *clfn_list;
+};
+
+/* for debuggers to be able to interrogate the generic type in the .h file */
+typedef ks_mpool_t ks_mpool_ext_t;
+
+/*
+ * Block header structure.  This structure *MUST* be long-word
+ * aligned.
+ */
+typedef struct ks_mpool_block_st {
+	unsigned int mb_magic;		/* magic number for block header */
+	void *mb_bounds_p;			/* block boundary location */
+	struct ks_mpool_block_st *mb_next_p;	/* linked list next pointer */
+	unsigned int mb_magic2;		/* upper magic for overwrite sanity */
+} ks_mpool_block_t;
+
+/*
+ * Free list structure.
+ */
+typedef struct {
+	void *mf_next_p;			/* pointer to the next free address */
+	unsigned long mf_size;		/* size of the free block */
+} ks_mpool_free_t;
 
 #ifndef MAP_ANON
 #define MAP_ANON MAP_ANONYMOUS
