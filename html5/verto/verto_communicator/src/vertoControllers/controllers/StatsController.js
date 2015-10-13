46263bdad62ff234c53aefa5be6c2d87daa25e0f
Ken Rice <krice@freeswitch.org>
2015-10-13 12:50:54 -0500

    wip

--- html5/verto/verto_communicator/src/vertoControllers/controllers/StatsController.js
+++ html5/verto/verto_communicator/src/vertoControllers/controllers/StatsController.js
@@ -6,41 +6,95 @@
 		.controller('StatsController', ['$scope', 'verto',
 				function($scope, verto) {
 					console.debug("stats window");
-					$scope.statsEnable = false;
 					$scope.peer = verto.data.call.rtc.peer;
-					$scope.videoBytes = [[0,0,0,0,0,0]];
-					$scope.videoLabels = [0,0,0,0,0,0];
-					$scope.series=["Video"];
+					$scope.videoBytes = [
+						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,]
+					];
+					$scope.videoLabels = ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""];
+					$scope.videoSeries=["Recieved", "Sent"];
 					$scope.statsTimeout = 0;
+					$scope.statsEnable = true;
+					$scope.lastVidSend = 0;
+					$scope.lastVidRecv = 0;
+					$scope.lastAudioSend = 0;
+					$scope.lastAudioRecv = 0;
+
 					getStats($scope.peer.peer);
+
 
 					function clickit(points, evt) {
 						$scope.statsEnable ^= 1;
-						if ( $scope.statsEnable ){
-							getStats($scope.peer.peer);
-						} else {
+						console.debug("$scope.statsEnable:" + $scope.statsEnable);
+						if ( !$scope.statsEnable ){
 							clearTimeout($scope.statsTimeout);
 						}
 					};
 
 					function getStats(pc) {
 						myGetStats(pc, function (results) {
-							
-							if ($scope.videoBytes.length >= 180) {
-								$scope.videoBytes.shift();
-							}
-							console.warn(results);
+							var lastPeriod = 0;
 							
 							//shit goes here to handle pushing stats into graph
+							results.forEach(function (a,b,c) {
+								// if (a.transportId == "Channel-audio-1" && a.bytesSent > 0) {
+								if (a.transportId == "Channel-video-1"){
+									//console.warn("video: ");
+									//console.warn(a);
+									if (a.bytesSent > 0){
+										console.warn("bytes [" + a.bytes + "] last bytes ["+ $scope.lastVidSend + "]");
+										if (a.bytesSent > $scope.lastVidSend) {
+											lastPeriod = a.bytesSent - $scope.lastVidSend;
+											console.warn("lastPeriod after math ["+lastPeriod+"]");
+										} else {
+											lastPeriod = a.bytesSent;
+											console.warn("lastPeriod No math ["+lastPeriod+"]");
+										}
+										$scope.lastVidSend = a.bytesSent;
+
+										var junk = $scope.videoBytes[1].shift();
+										$scope.videoBytes[1].push(lastPeriod);
+										console.warn(" after push $scope.videoBytes[1] :");
+										console.warn($scope.videoBytes[1]);
+									}
+									if (a.bytesReceived > 0) {
+										console.warn("bytes [" + a.bytes + "] last bytes ["+ $scope.lastVidRecv + "]");
+										if (a.bytesReceived > $scope.lastVidRecv) {
+											lastPeriod = a.bytesReceived - $scope.lastVidRecv;
+											console.warn("lastPeriod after math ["+lastPeriod+"]");
+										} else {
+											lastPeriod = a.bytesReceived;
+											console.warn("lastPeriod No math ["+lastPeriod+"]");
+										}
+										$scope.lastVidRecv = a.bytesReceived;
+
+										var junk = $scope.videoBytes[0].shift();
+										$scope.videoBytes[0].push(lastPeriod);
+										console.warn(" after push $scope.videoBytes[0] :");
+										console.warn($scope.videoBytes[0]);
+									}
+								}
+								if (a.transportId == "Channel-audio-1"){
+									console.warn("audio: ");
+									console.warn(a);
+									if (a.bytesSent > 0){
+										// do sent here
+
+									}
+									if (a.bytesReceived > 0) {
+										// received goes here
+										
+									}
+								}
+								// transportId: "Channel-video-1" && bytesReceived: "262921"
+
+							});
 
 							// $scope.videoBytes.push(results);
+							$scope.statsTimeout = setTimeout(function () {
+								getStats(pc);
+							}, 1000);
 							
-							if ( $scope.statsEnable ){
-								$scope.statsTimeout = setTimeout(function () {
-									getStats(pc);
-								}, 1000);
-							}
-
 						});
 					}
 
@@ -74,6 +128,15 @@
 						}
 					};
 					$scope.onClick = clickit;
+
+					$scope.$on(
+							"destroy",
+							function(event){
+								//$timeout.cancel($scope.statsTimeout);
+								clearTimeout($scope.statsTimeout);
+							}
+					);
+
 				}
 	]);
 })();
